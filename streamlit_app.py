f9f0ae976ad0a6d3175ef0b783d2c20a07f1c387
rapgoph <124239986+rapgoph@users.noreply.github.com>
2024-09-04 12:17:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,3 @@
-#import streamlit as st
-#import tempfile
-#import os
-
-#def main():
- #   st.title("Welcome! This is the Multimodal Emotion Recognition App! - Raphael and 명환!")
-
-    # File uploader allows users to upload a video file
-  #  video_file = st.file_uploader("Upload a video", type=["mp4", "mov", "avi"])
-
-   # if video_file is not None:
-        # Display the uploaded video
-    #    st.video(video_file)
-
-        # Save the uploaded video temporarily
-     #   with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as tmp_file:
-      #      tmp_file.write(video_file.read())
-       #     tmp_file_path = tmp_file.name
-
-        # Show some basic info about the video file
-        #st.write("### Video Information")
-        #st.write(f"**Filename:** {video_file.name}")
-        #st.write(f"**File Size:** {os.path.getsize(tmp_file_path)} bytes")
-
-        # Here you could add more analysis or processing on the video file
-
-#if __name__ == '__main__':
- #   main()
-
 import streamlit as st
 import whisper_timestamped as whisper
 import tempfile
